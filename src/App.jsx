7fc56fd6865515455848ac6ceaf8f6b66861938c
Duy Nguyen <duy.nguyenngoc@ncc.asia>
2022-05-26 16:23:58 +0700

    js for wheel

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 
-const list = Array(21)
+const list = Array(52)
   .fill(0)
   .map(() => Math.floor(Math.random() * 100));
-
-console.log('🚀 ~ file: App.jsx ~ line 6 ~ list', list);
 
 list[Math.floor(Math.random() * list.length)] = 101;
 
 function App() {
-  const [radius] = useState(75);
+  const [radius] = useState(130);
   const [rotate, setRotate] = useState(0);
-  const [easeOut] = useState(2);
+  const [easeOut, setEaseOut] = useState(4);
   const [result, setResult] = useState(null);
 
   useEffect(() => {
@@ -38,8 +36,8 @@ function App() {
     let startAngle = start;
     let endAngle = start + arc;
     let angle = index * arc;
-    let baseSize = wheelRadius * 3.33;
-    let textRadius = baseSize - 150;
+    let baseSize = radius * 2.3333;
+    let textRadius = baseSize - 70;
 
     ctx.beginPath();
     ctx.arc(x, y, wheelRadius, startAngle, endAngle, false);
@@ -67,44 +65,62 @@ function App() {
     return `rgba(${r},${g},${b},0.4)`;
   };
 
+  const getRndInteger = (min, max) => {
+    return Math.random() * (max - min) + min;
+  };
+
   const spin = () => {
+    // start position
+    const startPos = 38;
+
     const key = list.findIndex((item) => item === 101);
     let distance = 0;
-    if (key < 15) {
-      distance = 15 - key;
+    if (key < startPos) {
+      distance = startPos - key;
     } else {
-      distance = list.length - Math.abs(15 - key);
+      distance = list.length - startPos - key;
     }
 
     let randomSpin = (180 * distance * 2) / list.length;
 
-    setRotate(randomSpin + 360 * 3);
+    setRotate(randomSpin + 360 * 3 + getRndInteger(0, 360 / list.length));
+    setEaseOut(4);
 
     // calcalute result after wheel stops spinning
     setTimeout(() => {
       getResult(randomSpin);
-    }, 2000);
+    }, 4000);
   };
 
-  const getResult = (spin) => {
+  const getResult = () => {
     setResult(101);
+  };
+
+  const reset = () => {
+    setRotate(0);
+    setEaseOut(0);
   };
 
   return (
     <div className='App'>
-      <h1>Spinning Prize Wheel React</h1>
-      <span id='selector'>&#9660;</span>
-      <canvas
-        id='wheel'
-        width='500'
-        height='500'
-        style={{
-          WebkitTransform: `rotate(${rotate}deg)`,
-          WebkitTransition: `-webkit-transform ${easeOut}s ease-out`,
-        }}
-      />
+      <div className='wrapper'>
+        <span id='selector'>&#9660;</span>
+        <canvas
+          id='wheel'
+          width='600'
+          height='600'
+          style={{
+            WebkitTransform: `rotate(${rotate}deg)`,
+            WebkitTransition: `-webkit-transform ${easeOut}s ease-out`,
+            // transitionTimingFunction: 'cubic-bezier(1, 8, 1, 0.1)',
+          }}
+        />
+      </div>
       <button type='button' id='spin' onClick={spin}>
         spin
+      </button>
+      <button type='button' onClick={reset}>
+        reset
       </button>
       <div className='display'>
         <span id='readout'>
